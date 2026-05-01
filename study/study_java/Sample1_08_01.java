6985ac8d58c6f261c429630715f1fec34489134a
satosato <satouk0102@gmail.com>
2026-05-01 16:11:19 +0900

    数値文字列変換

---
+++ study/study_java/Sample1_08_01.java
@@ -0,0 +1,8 @@
+public class Sample1_08_01{
+    public static void main(String[] argv){
+        int inputNum = 10;
+        String inputStr = "10";
+        int outputNum = Integer.parseInt(inputStr);
+        String outputStr = String.valueOf(inputNum);
+    }
+}
