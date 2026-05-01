a98779fee68462d6c97f030cacf2eb1370bceb32
satosato <satouk0102@gmail.com>
2026-05-01 13:30:08 +0900

    セクション５対応

---
+++ study/study_java/Sample1_04_1.java
@@ -0,0 +1,9 @@
+class Sample1_04_1{
+    public static void main(String[] argv){
+        int teika = 1000;
+        double zeiritsu = 1.08;
+        double shiharai = teika * zeiritsu;
+
+        System.out.println((int)shiharai);
+    }
+}
